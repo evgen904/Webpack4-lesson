9559f496b3cb860b63b546a5439bd9f4318c06b9
Evgeniy Dzhurabaev <evgen904@gmail.com>
2018-10-05 22:47:48 +0300

    add Githab

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//import $ from 'jquery';
+//import avg from './some.js';
+import some from './some.js';
+
+//$('.title').html('Text primer');
+
+
+console.log(some.avg(1,4,8));
+console.log(some.max(1,4,8));
+console.log(some.merge({
+    a: 1
+},{
+    b: 2
+}));
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+let path = require('path');
+let conf = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'./dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = (env, options) => {
+    let production = options.mode === 'production';
+
+    conf.devtool = production
+        ? false
+        : 'eval-sourcemap';
+    return conf;
+}
